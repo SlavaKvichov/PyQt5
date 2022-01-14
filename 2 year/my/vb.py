0fc7f073308bd6d4af1581e27897f24b20a6070a
Rodion <rodiosha30018@gmail.com>
2022-01-14 18:01:17 +0200

    123

---
+++ 2 year/my/vb.py
@@ -0,0 +1,17 @@
+from time import *
+print("OMG!")
+sleep(0.8)
+print("It's a new people in this program...")
+sleep(2)
+print("(whispers: contrrol yourself, Jusper, control yourself)")
+sleep(2.3)
+print("Ohh.. he-he, hello, young user, what do you want to do in this so old program?")
+sleep(2.8)
+print('If you want know where you are just write "inf"')
+sleep(2.3)
+i = input('If you want exit the program just write "exit"')
+while i != 'exit':
+    if i == 'inf':
+        print("I'm friendly bot, my name is")
+    i = input('If you want exit the program just write "exit"')
+ij=input('Thank you, it was a pleasure talking with you, good bye.\n \nProcess finished with exit code 0')
