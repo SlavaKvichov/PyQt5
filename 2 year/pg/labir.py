0b0ea35579f26f22b6bd9a58afd7ac6fe69343bc
Rodion <rodiosha30018@gmail.com>
2022-01-19 20:22:40 +0200

    123

--- 2 year/pg/labir.py
+++ 2 year/pg/labir.py
@@ -31,11 +31,12 @@ while game:
 
     text1 = f1.render(str(o), True, (180, 0, 0))
 
-    if x1 < x2 < x1 + 65 and y1 < y2 < y1 + 65:
-        x2 = randint(0, 626)
-        y2 = randint(0, 426)
+    # if x1 < x2 < x1 + 65 and y1 < y2 < y1 + 65:
+    if x2 < x1 +100 and y2 < y1+100:
+        x2 = randint(10, 626)
+        y2 = randint(10, 426)
         o += 1
-    window.blit(background, (0, 0))
+    window.blit(backg round, (0, 0))
     window.blit(sprite1, (x1, y1))
     window.blit(sprite2, (x2, y2))
     window.blit(text2, (0, 0))
