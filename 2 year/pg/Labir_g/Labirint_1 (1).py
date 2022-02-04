14cf4bf3ebc16b8144f1181f50a8ba95a1ab97b0
Rodion <rodiosha30018@gmail.com>
2022-02-04 18:51:16 +0200

    123

--- 2 year/pg/Labir_g/Labirint_1 (1).py
+++ 2 year/pg/Labir_g/Labirint_1 (1).py
@@ -38,52 +38,18 @@ clock = time.Clock()
 FPS = 60
 game = True
 
-window_rez_x, window_rez_y = 1000, 666
+window_rez_x, window_rez_y = 1200, 800
 window = display.set_mode((window_rez_x, window_rez_y))
 display.set_caption("Лабиринт")
 
 background = transform.scale(image.load(os.path.join(BASE_DIR, 'background.jpg')), (window_rez_x, window_rez_y))
 
-hero = Player(os.path.join(BASE_DIR, 'hero.png'), 60, 20, 100, 5)
-enemy = Player(os.path.join(BASE_DIR, 'enemy.png'), 60, 800, 500, 4)
+hero = Player(os.path.join(BASE_DIR, 'hero.png'), 60, 100, 100, 5)
+enemy = Player(os.path.join(BASE_DIR, 'enemy.png'), 60, 1000, 600, 2)
 enemy.dir = 'right'
-target = Player(os.path.join(BASE_DIR, 'target.png'), 60, 850, 600, None)
+target = Player(os.path.join(BASE_DIR, 'target.png'), 60, 1050, 700, None)
 
-w1 = Wall(70, 130, 50, 20, 100, 0, 560)
-
-w2 = Wall(70, 130, 50, 20, 220, 366, 300)
-
-w3 = Wall(70, 130, 50, 20, 220, 100, 166)
-
-w4 = Wall(70, 130, 50, 150, 220, 100, 20)
-
-w5 = Wall(70, 130, 50, 20, 370, 0, 120)
-
-w6 = Wall(70, 130, 50, 270, 220, 266, 20)
-
-w7 = Wall(70, 130, 50, 20, 490, 100, 186)
-#
-w8 = Wall(70, 130, 50, 20, 610, 0, 120)
-
-w9 = Wall(70, 130, 50, 166, 610, 100, 20)
-#
-w10 = Wall(70, 130, 50, 20, 770, 100, 166)
-#
-w11 = Wall(70, 130, 50, 20, 340, 366, 200)
-#
-w12 = Wall(70, 130, 50, 450, 340, 366, 20)
-#
-w13 = Wall(70, 130, 50, 20, 630, 466, 200)
-#
-w14 = Wall(70, 130, 50, 160, 630, 466, 20)
-#
-w15 = Wall(70, 130, 50, 20, 770, 366, 120)
-
-w16 = Wall(70, 130, 50, 20, 890, 0, 486)
-
-w17 = Wall(70, 130, 50, 20, 630, 233, 20)
-
-w18 = Wall(70, 130, 50, 20, 490, 516, 20)
+w1 = Wall(140, 50, 10, 20, 400, 40, 500)
 
 mixer.init()
 mixer.music.load(os.path.join(BASE_DIR, '01389.mp3'))
@@ -99,23 +65,6 @@ while game:
     target.reset()
 
     w1.reset()
-    w2.reset()
-    w3.reset()
-    w4.reset()
-    w5.reset()
-    w6.reset()
-    w7.reset()
-    w8.reset()
-    w9.reset()
-    w10.reset()
-    w11.reset()
-    w12.reset()
-    w13.reset()
-    w14.reset()
-    w15.reset()
-    w16.reset()
-    w17.reset()
-    w18.reset()
 
     if keys_pressed[K_LEFT] and hero.rect.x > 5:
         hero.rect.x -= hero.speed
@@ -130,9 +79,9 @@ while game:
         enemy.rect.x += enemy.speed
     if enemy.dir == 'right' and enemy.rect.x >= window_rez_x - enemy.sprite_size:
         enemy.dir = 'left'
-    if enemy.dir == 'left' and enemy.rect.x > window_rez_x - 350:
+    if enemy.dir == 'left' and enemy.rect.x > window_rez_x - 300:
         enemy.rect.x -= enemy.speed
-    if enemy.dir == 'left' and enemy.rect.x <= window_rez_x - 350:
+    if enemy.dir == 'left' and enemy.rect.x <= window_rez_x - 300:
         enemy.dir = 'right'
 
     if sprite.collide_rect(hero, enemy):
